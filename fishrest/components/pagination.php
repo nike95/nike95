7bfed19308b875a4781230631829744f46847978
nike95 <67685319+nike95@users.noreply.github.com>
2021-01-04 21:49:48 +0200

    addfishrest
    add fishrest repositories

---
+++ fishrest/components/pagination.php
@@ -0,0 +1,21 @@
+<div class="pagination">
+	<div class="pagination__prev">
+		<svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M6 1L2 5.5L6 10" stroke="#DDA960" stroke-width="2"/>
+		</svg>
+	</div>
+	<ul class="pagination__list">
+		<li class="pagination__item"><a href="#">1</a></li>
+		<li class="pagination__item"><a href="#">...</a></li>
+		<li class="pagination__item"><a href="#">4</a></li>
+		<li class="pagination__item"><a href="#">5</a></li>
+		<li class="pagination__item"><a href="#">6</a></li>
+		<li class="pagination__item"><a href="#">...</a></li>
+		<li class="pagination__item"><a href="#">15</a></li>
+	</ul>
+	<div class="pagination__next">
+		<svg width="7" height="11" viewBox="0 0 7 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<path d="M6 1L2 5.5L6 10" stroke="#DDA960" stroke-width="2"/>
+		</svg>
+	</div>
+</div>
---
+++ fishrest/orderthanks.php
@@ -0,0 +1,12 @@
+<?php include('components/header.php') ?>
+	<main class="order">
+		<div class="container">
+			<div class="order__box">
+				<div class="order__icon"><img src="img/order/order.svg" alt=""></div>
+				<div class="order__title">Ваш заказ оформлен!</div>
+				<div class="order__subtitle">Начинаем собирать... Ничего не забыли? </div>
+				<a href="#" class="order__link">Перейти на главную страницу</a>
+			</div>
+		</div>
+	</main>
+<?php include('components/footer.php') ?>
