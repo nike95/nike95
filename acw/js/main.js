020746de17a9ccfcc27d7dbf6fc761c8d90a90d2
nike95 <67685319+nike95@users.noreply.github.com>
2021-02-12 11:39:14 +0200

    load new wersion site
    load new wersion site for new menu and soc

--- acw/js/main.js
+++ acw/js/main.js
@@ -8,15 +8,18 @@ $(function(){
 		$(".aboutproduct__img").removeClass("active");
 		$(".aboutproduct__img").eq(index).addClass("active");
 	});
-	
+
 	$('.burger').click(function(){
 		$(".min-menu").addClass("active");
-		$(".firstscreen").addClass("active");
-		$("body,html").addClass("noscroll");	
 	});
-	$('.min-menu__close').click(function(){
-		$(".min-menu").removeClass("active");
-		$(".firstscreen").removeClass("active");
-		$("body,html").removeClass("noscroll");	
-	});
+	$('.mysocial')
+      .on('click', function(e) {
+        $('.fab')
+            .removeClass('no');
+        if (e.target != this) return;
+        $('.mysocial, .fab')
+            .toggleClass('active');
+        $('.mysocial-bg')
+            .toggleClass('active');
+    });
 });
