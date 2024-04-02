4fb6a436f7bd10d1a18b1a65ce82feab6139b124
nike95 <67685319+nike95@users.noreply.github.com>
2024-04-02 23:36:20 +0300

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,10 @@
+addEventListener("DOMContentLoaded", (event) => {
+    let swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+});
