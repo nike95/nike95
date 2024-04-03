c18746ae36258c48f4584ce4206a23b3ce5c660d
nike95 <67685319+nike95@users.noreply.github.com>
2024-04-03 12:53:24 +0300

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,34 @@
+addEventListener("DOMContentLoaded", (event) => {
+    let swiper = new Swiper('.swiper-container', {
+        slidesPerView: 1,
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    const form = document.getElementById('registration-form');
+
+    form.addEventListener('submit', function(e) {
+        e.preventDefault();
+
+        const formData = new FormData(this);
+
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', 'https://nike95.github.io/test/submit');
+        xhr.onload = function() {
+            if (xhr.status === 200) {
+                alert('Данные отправлены успешно!');
+                form.reset();
+            } else {
+                alert('Произошла ошибка при отправке данных!');
+            }
+        };
+        xhr.onerror = function() {
+            alert('Произошла ошибка при отправке данных!');
+        };
+        xhr.send(formData);
+    });
+
+});
